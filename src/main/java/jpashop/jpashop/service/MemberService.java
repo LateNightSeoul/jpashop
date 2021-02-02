31633f313e8f98163a5981b429997d1f4944fe38
LateNightCoding <leehae8@naver.com>
2021-02-02 13:53:30 +0900

    feat: 회원가입 비즈니스 로직 구현

---
+++ src/main/java/jpashop/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package jpashop.jpashop.service;
+
+import jpashop.jpashop.domain.Member;
+import jpashop.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMmeber(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMmeber(Member member) {
+        List<Member> findmembers = memberRepository.findByName(member.getName());
+        if(!findmembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOnd(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
