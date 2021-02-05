34ed6d060a044baeb6bdf3db8ce60994c7378117
LateNightCoding <leehae8@naver.com>
2021-02-05 17:42:28 +0900

    feat : 주문 내역 검색 기능 구현

---
+++ src/main/java/jpashop/jpashop/repository/OrderSearch.java
@@ -0,0 +1,17 @@
+package jpashop.jpashop.repository;
+
+import jpashop.jpashop.domain.Order;
+import jpashop.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
